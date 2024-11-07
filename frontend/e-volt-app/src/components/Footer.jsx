87ebce26ed0af2d5048d9c4c2e8c88be9b2ef8f6
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2024-11-07 17:43:27 +0530

    Update Footer.jsx

--- frontend/e-volt-app/src/components/Footer.jsx
+++ frontend/e-volt-app/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from "next/link";
 
 export default function Footer() {
+  
   return (
     <footer className='bg-[#FAFAFA] w-screen py-8 px-20 mt-20'>
         <div className='flex items-center justify-start border-b-2 pb-4'>
